2ef4f4ef9921e3cf756739ddcfb19098748bbd63
Alexandra <a.chigrina89@gmail.com>
2024-12-02 13:21:31 +0300

    function get_date commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-card_number_input = int(input())
-print(get_mask_card_number(card_number_input))
+# card_number_input = int(input())
+# print(get_mask_card_number(card_number_input))
+#
+#
+# account_number_input = int(input())
+# print(get_mask_account(account_number_input))
 
 
-account_number_input = int(input())
-print(get_mask_account(account_number_input))
-
+from src.widget import mask_account_card
 
 # Maestro 1596837868705199
 # Счет 64686473678894779589
@@ -16,3 +18,10 @@ print(get_mask_account(account_number_input))
 # Visa Platinum 8990922113665229
 # Visa Gold 5999414228426353
 # Счет 73654108430135874305
+
+# print(mask_account_card('Счет 7365410843013587'))
+
+
+from src.widget import get_date
+
+# print(get_date('2024-03-11T02:26:18.671407'))
--- src/widget.py
+++ src/widget.py
@@ -10,6 +10,20 @@ def mask_account_card(user_data: str) -> str:
         return user_data[:-16] + get_mask_card_number(int(user_data_list[-1]))
     elif len(user_data_list[-1]) == 20:
         return user_data[:-20] + get_mask_account(int(user_data_list[-1]))
+    else:
+        raise ValueError("Неверная длина номера карты/счета")
+
+
+def get_date(date_data: str) ->str:
+    """
+    Возвращает строку с датой в формате 'ДД.ММ.ГГГГ'
+    """
+    return f'{date_data[8:10]}.{date_data[5:7]}.{date_data[0:4]}'
+
+
+
+
+
 
 
 
