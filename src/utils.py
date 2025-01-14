d55b60a23a48a3362a0f7e86bc56a44963975060
Alexandra <a.chigrina89@gmail.com>
2025-01-15 02:30:47 +0300

    add cov-test plus fixes

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,14 @@ import json
 import os.path
 
 
-def get_financial_transactions(path: str) -> list[dict] :
+def get_financial_transactions(path: str) -> list[dict]:
     """
     Принммает на вход путь до JSON-файла, возвращает список словарей с данными
     о финансовых транзакциях
     """
 
     try:
-        with open(path, 'r', encoding='utf-8') as file:
+        with open(path, "r", encoding="utf-8") as file:
             try:
                 transactions = json.load(file)
                 if isinstance(transactions, list):
@@ -22,7 +22,7 @@ def get_financial_transactions(path: str) -> list[dict] :
         return []
 
 
-path_to_file = os.path.join(os.path.dirname(__file__), '..', 'data', 'operations.json')
+path_to_file = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
 
 
 # if __name__ == '__main__':
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,7 +1,9 @@
+from unittest.mock import Mock, patch
+
+import pytest
+
 from src.external_api import get_transaction_amount
 from src.utils import get_financial_transactions, path_to_file
-from unittest.mock import patch, Mock
-import pytest
 
 
 def test_get_trans_amount_RUB():
@@ -17,15 +19,16 @@ def test_get_transaction_amount():
         "to": "RUB",
         "amount": 8221.37,
         "rate": 103.369018,
-        "result": 849834.943515
+        "result": 849834.943515,
     }
 
-    with patch('requests.get', return_value=mock_response):
-        test_transaction = {'id': 41428829,
-                            'state': 'EXECUTED',
-                            'date': '2019-07-03T18:35:29.512364',
-                            'operationAmount': {'amount': '8221.37',
-                                                'currency': {'name': 'USDT', 'code': 'USD'}}}
+    with patch("requests.get", return_value=mock_response):
+        test_transaction = {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {"amount": "8221.37", "currency": {"name": "USDT", "code": "USD"}},
+        }
         result = get_transaction_amount(test_transaction)
         assert result == 849834.943515
 
@@ -34,11 +37,12 @@ def test_get_trans_amount_no_currency():
     mock_response = Mock()
     mock_response.status_code = 400
 
-    with patch('requests.get', return_value=mock_response):
+    with patch("requests.get", return_value=mock_response):
         with pytest.raises(ValueError):
-            test_transaction = {'id': 41428829,
-                                'state': 'EXECUTED',
-                                'date': '2019-07-03T18:35:29.512364',
-                                'operationAmount': {'amount': '132.47',
-                                                    'currency': {'name': 'USDT', 'code': 'USDT'}}}
+            test_transaction = {
+                "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019-07-03T18:35:29.512364",
+                "operationAmount": {"amount": "132.47", "currency": {"name": "USDT", "code": "USDT"}},
+            }
             get_transaction_amount(test_transaction)
